7cd0e99d1d5aa7d97a8e9ba76021e70c942d7fd9
Andy Guinder <andreabelen.guinder@gmail.com>
2026-07-18 00:34:29 -0300

    cambio algunas cuotas y empiezo con firebase

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDtmVf-scvqH2syqOkqCxm161gM53vbhvc",
+  authDomain: "forge-tech-7a44c.firebaseapp.com",
+  projectId: "forge-tech-7a44c",
+  storageBucket: "forge-tech-7a44c.firebasestorage.app",
+  messagingSenderId: "254229374943",
+  appId: "1:254229374943:web:5f16089ad039bdd454c66a",
+  measurementId: "G-Y1Z7M29VVV"
+};
+
+// Inicializar Firestore
+const db = getFirestore(app);
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Exportar la instancia de Firestore para usar en componentes
+export { db };
